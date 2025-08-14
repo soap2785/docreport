b5d6511c20c68b1002833ac23a2befda045c5103
Soap2785 <76537824+CyberArtemida2468@users.noreply.github.com>
2025-08-14 18:19:10 +0300

    modded civil_service.py, bankrupts.py

--- business_logic/parsers/bankrupts.py
+++ business_logic/parsers/bankrupts.py
@@ -1,12 +1,9 @@
 import time
 
 from selenium import webdriver
-from selenium.common import ElementNotInteractableException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from config import proxies
 
