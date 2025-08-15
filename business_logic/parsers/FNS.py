d7790838435932ffb31265876dfc48190d6a4739
Soap2785 <76537824+CyberArtemida2468@users.noreply.github.com>
2025-08-15 22:35:06 +0300

    modded FNS

--- business_logic/parsers/FNS.py
+++ business_logic/parsers/FNS.py
@@ -1,5 +1,4 @@
 import time
-from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
@@ -14,7 +13,7 @@ from config import proxies
 
 webdriver.DesiredCapabilities.CHROME['proxy'] = proxies
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 driver = webdriver.Chrome(options=chrome_options)
